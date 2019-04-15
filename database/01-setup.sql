e816fb7921705097206a92f59dcf86bf36f778be
alaomichael <contactleomax@gmail.com>
2019-04-15 18:46:36 +0100

    New Update 4

--- database/01-setup.sql
+++ database/01-setup.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 27, 2017 at 12:35 PM
--- Server version: 5.7.19-0ubuntu0.16.04.1
--- PHP Version: 7.0.22-0ubuntu0.16.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Apr 15, 2019 at 07:41 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `login_course`
 --
-CREATE DATABASE IF NOT EXISTS `login_course` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `login_course`;
 
 -- --------------------------------------------------------
 
@@ -28,19 +28,13 @@ USE `login_course`;
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `user_id` int(5) UNSIGNED NOT NULL COMMENT 'User Id',
-  `email` varchar(190) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Users email address',
-  `password` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User password',
-  `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The time and date the user registered'
+  `user_id` int(5) UNSIGNED NOT NULL COMMENT 'User id',
+  `email` varchar(190) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'User email address',
+  `password` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time and date the user registered'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Users table';
 
---
--- Truncate table before insert `users`
---
-
-TRUNCATE TABLE `users`;
 --
 -- Indexes for dumped tables
 --
@@ -60,7 +54,9 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Id';
+  MODIFY `user_id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User id';
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
